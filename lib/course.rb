20ee55d3835a3a2142bbd114e6e640f642113565
Matt Farmer <mrfarmer777@gmail.com>
2018-06-01 05:48:10 +0000

    automatically backed up by learn

--- lib/course.rb
+++ lib/course.rb
@@ -2,12 +2,16 @@ class Course
   @@all=[]
   attr_accessor :title, :schedule, :description
 
-  def initialize    
+  def initialize
     @@all<<self
   end
 
   def self.all
     @@all
+  end
+
+  def reset_all
+    @@all.clear
   end
 
 end
